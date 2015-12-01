bafff55fbdc4dfa9f8412d076061ffe9c905e6a4
seungdols <choiseungho0822@gmail.com>
2015-12-01 11:19:41 +0900

    add files

---
+++ src/com/tistory/seungdols/Main.java
@@ -0,0 +1,16 @@
+package com.tistory.seungdols;
+
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	private static BorderPane root = new BorderPane();
+	@Override
+	public void start(Stage arg0) throws Exception {
+		 
+
+	}
+
+}
