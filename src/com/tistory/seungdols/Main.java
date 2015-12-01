b4a9ea8c38c1625e25fb339dbb330e5e299d71f5
seungdols <choiseungho0822@gmail.com>
2015-12-01 11:26:16 +0900

    Scene 하나에 FXML 두개 로드 시키기

--- src/com/tistory/seungdols/Main.java
+++ src/com/tistory/seungdols/Main.java
@@ -1,6 +1,12 @@
 package com.tistory.seungdols;
 
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -8,9 +14,30 @@ public class Main extends Application {
 	
 	private static BorderPane root = new BorderPane();
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 		 
+		primaryStage.setTitle("Text JavaFx");
+		
+		URL menubarURL = getClass().getResource("./resource/menubar.fxml");//get path of fxml file
+		MenuBar menubar = FXMLLoader.load(menubarURL);//load fxml
+		
+		root.setTop(menubar);
+		
+		URL paneURL = getClass().getResource("./resource/pane.fxml");//get path of fxml file
+		Parent pane = FXMLLoader.load(paneURL);
+		
+		root.setCenter(pane);
+		
+		Scene main_view  = new Scene(root);
+		
+		primaryStage.setScene(main_view);
+		primaryStage.setResizable(false);
+		primaryStage.centerOnScreen();
+		primaryStage.show();
+		
 
 	}
-
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
